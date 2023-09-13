dfe401c38962b021c6e185c524628b7428e9fa52
skiesfallen <sestayt@usm.cl>
2023-09-13 16:55:26 -0300

    cambios ejecutados en revision

--- Pagina_Web/Pagina_Web/views.py
+++ Pagina_Web/Pagina_Web/views.py
@@ -2,8 +2,9 @@
 # HttpResponse: Enviar respuesta usando metodos http
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def prueba(request):
-    return HttpResponse("Prueba")
+    return HttpResponse("Saludos a la fer porque llego TARDE")
 def default(request):
     return HttpResponse("Aqui descifrando los misterios de Django, salu2")
