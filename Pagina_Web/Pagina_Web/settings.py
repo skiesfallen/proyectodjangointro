3f93f148c89bcaf66a783ac752f693fa979f0916
poloyuyu <jaimeinostrozapoblete@gmail.com>
2023-09-24 15:34:14 -0300

    Solucion

--- Pagina_Web/Pagina_Web/settings.py
+++ Pagina_Web/Pagina_Web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Pagina_Web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r"C:\Users\jaime\OneDrive\Escritorio\Codigos\Proyecto intro ingenieria\Pagina_Web\Pagina_Web\template"],
+        'DIRS': [r"Pagina_Web\template"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'Pagina_Web/static'),)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- Pagina_Web/Pagina_Web/urls.py
+++ Pagina_Web/Pagina_Web/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from Pagina_Web.views import prueba, default
 
 urlpatterns = [
@@ -24,4 +26,4 @@ urlpatterns = [
     path('',default),
 
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
