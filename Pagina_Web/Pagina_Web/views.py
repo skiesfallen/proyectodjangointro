9ca882aa4adc70c8f0fa5e2a66262c6d528bef10
skiesfallen <sestayt@usm.cl>
2023-09-27 16:28:17 -0300

    push map y capetas

--- Pagina_Web/Pagina_Web/views.py
+++ Pagina_Web/Pagina_Web/views.py
@@ -5,11 +5,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def mapabox(request):
-    return render(request, 'miapp/mapa.html')
 def prueba(request):
     return HttpResponse("Saludos a la fer porque llego TARDE")
 def default(request):
-    return render(request,"default/index.html")
+    return render(request,"index.html")
 def mapa(request):
-    return render(request,"mapa/mapa.html")
+    return render(request,"mapa.html")
