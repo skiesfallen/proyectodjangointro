68d8b6fc72ac897274ff569407d2b816f415187c
skiesfallen <sestayt@usm.cl>
2023-09-27 15:17:18 -0300

    prueba mapbox

--- Pagina_Web/Pagina_Web/views.py
+++ Pagina_Web/Pagina_Web/views.py
@@ -4,6 +4,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
+def mapabox(request):
+    return render(request, 'miapp/mapa.html')
 def prueba(request):
     return HttpResponse("Saludos a la fer porque llego TARDE")
 def default(request):
