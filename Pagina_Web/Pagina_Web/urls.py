a54b071d59c7a6f654f62db1bb262d9d4bc9ea5c
cloudy0187 <fernandavicencio261@gmail.com>
2023-10-23 11:11:02 -0300

    asdfg

--- Pagina_Web/Pagina_Web/urls.py
+++ Pagina_Web/Pagina_Web/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from Pagina_Web.views import prueba, mapa, modismos, index, LogIn, Register
+from Pagina_Web.views import prueba, mapa, modismos, index, LogIn, Register, Modissmo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
