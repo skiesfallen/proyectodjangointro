770829229fc4ad73070890090cd3defe323230f1
poloyuyu <jaimeinostrozapoblete@gmail.com>
2023-10-30 17:02:38 -0300

    Pasaron cosas

---
+++ Pagina_Web/Pagina_Web/static/js/Map/config.js
@@ -0,0 +1,1 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2VzdGF5dCIsImEiOiJjbG4yMjdnNXcwM3huMnFuaGQ0b2pvZmNjIn0.JaDwRJbv84xiaL48MmZDkg';
---
+++ Pagina_Web/Pagina_Web/static/js/Map/script.js
@@ -0,0 +1,14 @@
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-71.59496643495604,-33.035960040525815], // Coordenadas iniciales del mapa (UTFSM)
+    zoom: 16.5 // Nivel de zoom inicial
+});
+map.addControl(new mapboxgl.NavigationControl());
+
+map.addControl(new mapboxgl.GeolocateControl({
+    positionOption:{
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true
+}))
