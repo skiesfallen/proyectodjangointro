1b3433266e9491833e3dac0ccc9d3ca23a15634f
poloyuyu <jaimeinostrozapoblete@gmail.com>
2023-11-26 14:45:30 -0300

    Debug = True Es una loca, me manda vidios al snap mientras se toca, me  dice que si h

--- Pagina_Web/Pagina_Web/settings.py
+++ Pagina_Web/Pagina_Web/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2t)w2(ohg^r6m46dbwd5ql$iafpzwdv&7unq*bw^g5rr(bi^fc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -116,14 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-if DEBUG:
-  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'Pagina_Web/static'),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
